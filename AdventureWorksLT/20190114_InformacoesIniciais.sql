d5577cc27814790f590eba0aaacba4696ef6482b
Gabriel W. Galdino <gawiga@gmail.com>
2019-01-14 20:32:55 -0200

    Add first script

---
+++ AdventureWorksLT/20190114_InformacoesIniciais.sql
@@ -0,0 +1,17 @@
+USE AdventureWorksLT2016;
+
+-- traz todas as tabelas da base
+SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE'
+
+/* SCHEMA: SalesLT
+Address
+Customer
+CustomerAddress
+Product
+ProductCategory
+ProductDescription
+ProductModel
+ProductModelProductDescription
+SalesOrderDetail
+SalesOrderHeader
+*/
